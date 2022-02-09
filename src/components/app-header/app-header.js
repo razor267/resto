7c6e0646c6afe60a653b2976a0c21ea9ff444e59
Baiborin Petr <razor267@mail.ru>
2022-02-09 22:16:02 +0300

    DZ LES 110

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import cartIcon from './shopping-cart-solid.svg';
 import './app-header.scss';
+import {Link} from "react-router-dom";
 
 const AppHeader = ({total}) => {
     return (
         <header className="header">
-            <a className="header__link" href="#">
-                Menu
-            </a>
-            <a className="header__link" href="#">
-                <img className="header__cart" src={cartIcon} alt="cart"></img>
-                Total: {total} $
-            </a>
+            <Link to='/menu'>
+                <div className="header__link">
+                    Menu
+                </div>
+            </Link>
+            <Link to='/cart'>
+                <div className="header__link">
+                    <img className="header__cart" src={cartIcon} alt="cart"></img>
+                    Total: {total} $
+                </div>
+            </Link>
         </header>
     )
 };
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import {MainPage, CartPage} from '../pages';
+import {CartPage, MainPage} from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from "../hoc";
 
 import Background from './food-bg.jpg';
+import {Route, Switch} from "react-router-dom";
 
 const App = ({RestoService}) => {
     console.log(RestoService.getMenuItems());
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+            <Switch>
+                <Route path='/' exact component={() => <div className="menu__list"></div>}/>
+                <Route path='/menu' component={MainPage}/>
+                <Route path='/cart' component={CartPage}/>
+            </Switch>
         </div>
     )
 }
